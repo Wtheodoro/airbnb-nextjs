0653f193cf3919080a738a797b22b3b1240f71b8
Walison <theodoro.walison@gmail.com>
2022-01-02 10:54:29 -0300

    feat: live anywhere section

---
+++ components/MediumCard/index.js
@@ -0,0 +1,23 @@
+import Image from "next/image"
+
+// WHEN USING LAYOUT FILL FROM IMAGE/NEXT COMPONENT, THE PARENT DIV MUST BE POSITION RELATIVE.
+// otherwise the image will fill the hole page
+// parent div must have the dimensions defined
+
+const MediumCard = ({ img, title }) => {
+
+  return (
+    <div className="cursor-pointer hover:scale-105 transform transition duration-300 ease-out">
+      <div className="relative h-80 w-80">
+        <Image 
+          src={img}
+          layout="fill"
+          className="rounded-xl"
+        />
+      </div>
+      <h3 className="text-2xl mt-3">{title}</h3>
+    </div>
+  )
+}
+
+export default MediumCard
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
+import SmallCard from '../components/SmallCard'
+import MediumCard from '../components/MediumCard'
 
-const Home = () => {
+// ON NEXT WE HAVE STATIC RENDER AND SERVER SIDE RENDER,
+// on this home page the data don change so much and thtas why,
+// i'll use static render.
+
+const Home = ({ exploreData, cardsData }) => {
   return (
     <div className="">
       <Head>
@@ -12,9 +18,60 @@ const Home = () => {
       </Head>
 
       <Header />
+
       <Banner />
+
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {
+              exploreData?.map(({ img, distance, location}) => (
+                <SmallCard
+                  key={`${img}_${location}`}
+                  img={img}
+                  distance={distance}
+                  location={location}
+                />
+              ))
+            }
+          </div>
+        </section>
+
+        <section>
+          <h2 className='text-4xl font-semibold py-5'>Live Anywhere</h2>
+
+          <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+            {
+              cardsData?.map(({ img, title }) => (
+                <MediumCard
+                  key={`${img}_${title}`}
+                  img={img}
+                  title={title}
+                />
+              ))
+            }
+          </div>
+        </section>
+      </main>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const exploreData = await fetch('https://links.papareact.com/pyp')
+    .then((response) => response.json())
+
+    const cardsData = await fetch('https://links.papareact.com/zp1')
+    .then((response) => response.json())
+
+  return {
+    props: {
+      exploreData,
+      cardsData
+    }
+  }
 }
 
 export default Home
