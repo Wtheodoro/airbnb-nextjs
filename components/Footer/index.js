570e60efa0d792cebc4e13526941fef8d76f9522
Walison <theodoro.walison@gmail.com>
2022-01-06 10:04:12 -0300

    feat: add contact link

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,9 +3,14 @@ const Footer = () => {
   return (
     <footer className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
       <div className="space-y-4 text-xs text-gray-800">
-        <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb works</p>
-        <p>Newsroom</p>
+        <h5 className="font-bold">CONTACT</h5>
+        <p>Walison developer</p>
+        <a href="https://www.linkedin.com/in/walisonteodoro/" rel="noopener noreferrer">
+          <p className="cursor-pointer mt-4">Linkedin</p>
+        </a>
+        <a href="https://github.com/Wtheodoro" rel="noopener noreferrer">
+          <p className="cursor-pointer mt-4">Github</p>
+        </a>
         <p>Investors</p>
         <p>Airbnb Plus</p>
         <p>Airbnb Luxe</p>
