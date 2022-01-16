3252da8edb77f68f3b1f49d9430978f414140e7e
Walison <theodoro.walison@gmail.com>
2022-01-16 10:00:50 -0300

    fix: banner image

--- components/Banner/index.js
+++ components/Banner/index.js
@@ -6,14 +6,14 @@ const Banner = () => {
   return (
     <div className="relative mx-auto w-11/12 h-[300px] sm:h-[400px] lg:h-[500px] xl-h[600px] 2xl:h-[700px]">
       <Image
-        src="https://links.papareact.com/0fm"
+        src="https://cdn.pixabay.com/photo/2016/11/19/14/30/aurora-borealis-1839582_1280.jpg"
         layout="fill"
         objectFit="cover"
         className="rounded-2xl"
         alt=""
       />
       <div className="absolute top-1/2 w-full text-center">
-        <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+        <p className="text-white text-sm sm:text-lg">Not sure where to go? Perfect.</p>
         <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">I&apos;m flexible</button>
       </div>
     </div>
--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['links.papareact.com'],
+    domains: [
+      'links.papareact.com',
+      'cdn.pixabay.com',
+    ],
   },
   env: {
     MAPBOX_KEY: 'pk.eyJ1Ijoid3Rlb2Rvcm8iLCJhIjoiY2t5MzAyb3VwMDl2MzJ3dWl1bG4xb3F5eCJ9.wQ1SF50EAeEOG7QQduF2iA'
